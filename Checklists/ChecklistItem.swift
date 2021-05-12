f06d76e7dcfe168006a676443418308e7df68cf2
dew613 <dewansunnah613@gmail.com>
2021-05-12 02:30:39 -0400

    finished part 1 of checklists

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,14 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Dewan Sunnah on 5/11/21.
+//
+
+import Foundation
+
+class ChecklistItem: NSObject, Codable {
+    var text = ""
+    var checked = false
+    
+}
